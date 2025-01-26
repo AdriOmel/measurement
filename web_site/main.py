36d84bafa722a3352ea1fc2d2d07899eb25de2e8
AdriOmel <adriana.omelchenko@student.nhlstenden.com>
2025-01-26 20:33:49 +0100

    add route update post

--- web_site/main.py
+++ web_site/main.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 @app.route("/")
 def hello():
   return "Hello World!"
   
+@app.route("/update", methods = ["POST"])
+def record_data():
+  content = request.json
+  print(content)
+  return None, 204
+
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", port = 8080)
